6c0e7e7fac482ca0dfac9927575709ba4eac71f6
Sander Meijer <sandertje1972@hotmail.com>
2021-06-07 09:45:33 +0200

    changed tests

--- functions.test.js
+++ functions.test.js
@@ -10,53 +10,53 @@ const {
   //Not null
 
 test('no input is not accepted', () => {
-    expect(isNotNull(null)).toBeFalsy();    
+    expect(isNotNull(null)).toBe(false);    
 });
 
 test('input is accepted', () => {
-    expect(isNotNull("henkie1")).toBeTruthy();
+    expect(isNotNull("henkie1")).toBe(true);
 });
     
 
 //Right Length
 
 test('henkie1234 has not the right length', () => {
-    expect(hasRightLength("henkie1234")).toBeFalsy();
+    expect(hasRightLength("henkie1234")).toBe(false);
 });
 
 test('henkie1 has the right length', () => {
-    expect(hasRightLength("henkie1")).toBeTruthy();
+    expect(hasRightLength("henkie1")).toBe(true);
 });
 
 
 //UpperCase Character
 
 test('henkie1 has no uppercase character', () => {
-    expect(hasUpperCaseCharacter("henkie1")).toBeFalsy();
+    expect(hasUpperCaseCharacter("henkie1")).toBe(false);
 });
 
 test('Henkie1 has uppercase character', () => {
-    expect(hasUpperCaseCharacter("Henkie1")).toBeTruthy();
+    expect(hasUpperCaseCharacter("Henkie1")).toBe(true);
 });
 
 
 //LowerCase Character
 
 test('HENKIE1 has no lowercase character', () => {
-    expect(hasLowerCaseCharacter("HENKIE1")).toBeFalsy();
+    expect(hasLowerCaseCharacter("HENKIE1")).toBe(false);
 });
 
 test('Henkie1 has lowercase character', () => {
-    expect(hasLowerCaseCharacter("Henkie1")).toBeTruthy();
+    expect(hasLowerCaseCharacter("Henkie1")).toBe(true);
 });
 
 
 //Number
 
 test('Henkie has no number', () => {
-    expect(hasDigit("Henkie")).toBeFalsy();
+    expect(hasDigit("Henkie")).toBe(false);
 });
 
 test('Henkie1 has a number', () => {
-    expect(hasDigit("Henkie1")).toBeTruthy();
+    expect(hasDigit("Henkie1")).toBe(true);
 });

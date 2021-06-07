172ea0cd71500780c3e284e0e73452a4f0a4792b
Sander Meijer <sandertje1972@hotmail.com>
2021-06-07 08:41:35 +0200

    added tests

--- functions.js
+++ functions.js
@@ -1,6 +1,17 @@
-const isNotNull = (str) => str !== null;
+const isNotNull = (password) => password !== null;
 
-const hasRightLength = (str) => str.length <= 8;
+const hasRightLength = (password) => password.length <= 8;
 
-module.exports = isNotNull;
-module.exports = hasRightLength;
+const hasUpperCaseCharacter = (password) => /[A-Z]/.test(password);
+
+const hasLowerCaseCharacter = (password) => /[a-z]/.test(password);
+
+const hasDigit = (password) => /[0-9]/.test(password);
+
+module.exports = {
+    isNotNull,
+    hasRightLength,
+    hasUpperCaseCharacter,
+    hasLowerCaseCharacter,
+    hasDigit,
+  };
--- functions.test.js
+++ functions.test.js
@@ -1,19 +1,62 @@
-const isNotNull = require('./functions.js');
+const {
+    isNotNull,
+    hasRightLength,
+    hasUpperCaseCharacter,
+    hasLowerCaseCharacter,
+    hasDigit,
+  } = require("./functions.js");
 
-test('check if password is not null', () => {
-    expect(isNotNull(null)).toBe(false);    
+
+  //Not null
+
+test('no input is not accepted', () => {
+    expect(isNotNull(null)).toBeFalsy();    
 });
 
-test('check if password is not null, happy path', () => {
-    expect(isNotNull("a")).toBe(true);
+test('input is accepted', () => {
+    expect(isNotNull("henkie1")).toBeTruthy();
 });
-
-const hasRightLength = require('./functions.js');
     
-test('check if password is not too long', () => {
-    expect(hasRightLength("abcdefghi")).toBe(false);
+
+//Right Length
+
+test('henkie1234 has not the right length', () => {
+    expect(hasRightLength("henkie1234")).toBeFalsy();
 });
 
-test('check if password is not too long, happy path', () => {
-    expect(hasRightLength("abc")).toBe(true);
+test('henkie1 has the right length', () => {
+    expect(hasRightLength("henkie1")).toBeTruthy();
+});
+
+
+//UpperCase Character
+
+test('henkie1 has no uppercase character', () => {
+    expect(hasUpperCaseCharacter("henkie1")).toBeFalsy();
+});
+
+test('Henkie1 has uppercase character', () => {
+    expect(hasUpperCaseCharacter("Henkie1")).toBeTruthy();
+});
+
+
+//LowerCase Character
+
+test('HENKIE1 has no lowercase character', () => {
+    expect(hasLowerCaseCharacter("HENKIE1")).toBeFalsy();
+});
+
+test('Henkie1 has lowercase character', () => {
+    expect(hasLowerCaseCharacter("Henkie1")).toBeTruthy();
+});
+
+
+//Number
+
+test('Henkie has no number', () => {
+    expect(hasDigit("Henkie")).toBeFalsy();
+});
+
+test('Henkie1 has a number', () => {
+    expect(hasDigit("Henkie1")).toBeTruthy();
 });
